c6977feae236821a0306f2324d4b64c9041270bc
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-09-30 10:44:26 -0500

    1. Added other conditions for test_score when out of the range (it made the other 3 tests pass).
    2. Added a main function to pull user input
    
    3. Added detailed comments and docstring to program.

--- more_functions/validate_input_in_functions.py
+++ more_functions/validate_input_in_functions.py
@@ -1,18 +1,33 @@
 """
 Grayson Hardin
 Program: validate_input_in_function.py
-Date last modified: 9/29/2020
+Date last modified: 9/30/2020
 
-This program will ask for two user inputs: a test name and a test score. The inputs have conditions and limits. If the input is not within the limits (range) it will loop until met.
+This program will ask for two user inputs: a test name and a test score. The inputs have conditions and limits. If the input is not within the limits (range) it will loop until met. Once it has been met, it will print the two values in a formatted message.
+
+The first parameter is test_name: It will take an input from the user and does not require validation.
+The second parameter is test_score: The default value has been assigned to 0 and allows the user to input their score. If the user chooses a number less than 0 or a value greater than 100, it will notify the user of invalid input and ask again.
+The third parameter is invalid_message: The default value has been assigned to, "Invalid test score, try again!" That message is given every time a invalid value is inputted (i.e: -1 or 101).
 """
 
 
-def score_input(test_name,test_score=0, invalid_message='Invalid test score, try again!'):
-    if isinstance(test_score, str):
+def score_input(test_name, test_score=0, invalid_message='Invalid test score, try again!'):  # Parameter values given.
+    if isinstance(test_score, str) or test_score < 0 or test_score > 100:  # Here we define the condition and limitations of the error message.
         raise ValueError(invalid_message)
 
-    return f'{test_name}: {test_score}'
+    return f'{test_name}: {test_score}'  # Returns the test_name and test_score in a formatted string
+
+
+def main():
+    try:
+        user_test_name_input = input('Enter a test name: ')  # Gain input for test name
+        user_test_score_input = int(input('Enter a test score: '))  # Gain input for test score
+        results = score_input(user_test_name_input, user_test_score_input)  # Calculation of the two previous values
+        print(results)
+    except ValueError as err:
+        print('Error: ', err)  # If an error occurs, it will print "Error" along with the specific message.
+        main()
 
 
 if __name__ == '__main__':
-    score_input()
+    main()
--- test_functions/test_valid_input_in_functions.py
+++ test_functions/test_valid_input_in_functions.py
@@ -3,14 +3,14 @@ from more_functions.validate_input_in_functions import score_input
 
 
 class MyTestCase(unittest.TestCase):
-    def test_score_input_test_name(self):
+    def test_score_input_test_name(self):  # Ultimately, this is what I want to return: a formatted message with the test_name and a proper integer value
         test_name = 'Python Exam 101'
         expected = f'{test_name}: {0}'
 
         actual = score_input(test_name)
         self.assertEqual(expected, actual)
 
-    def test_score_input_test_score_valid(self):
+    def test_score_input_test_score_valid(self):  # This test provides a good example of what a proper scenario should be
         test_name = 'Python Exam 101'
         test_score = 90
 
@@ -19,14 +19,14 @@ class MyTestCase(unittest.TestCase):
 
         self.assertEqual(expected, actual)
 
-    def test_score_input_test_score_below_range(self):
+    def test_score_input_test_score_below_range(self):  # This test provides a scenario for if the test_score is under the range
         with self.assertRaises(ValueError):
             test_name = 'Python Exam 101'
             test_score = -1
 
             score_input(test_name, test_score)
 
-    def test_score_input_test_score_above_range(self):
+    def test_score_input_test_score_above_range(self):  # This test provides a scenario for if the test_score is above the range
         with self.assertRaises(ValueError) as err:
             test_name = 'Python Exam 101'
             test_score = 101
@@ -34,7 +34,7 @@ class MyTestCase(unittest.TestCase):
             score_input(test_name, test_score)
         self.assertTrue('Invalid test score, try again!' in str(err.exception))
 
-    def test_score_input_non_numeric(self):
+    def test_score_input_non_numeric(self):  # Notice that for test_score I have added a section to validate whether the value is a string or integer
         with self.assertRaises(ValueError) as err:
             test_name = 'Python Exam 101'
             test_score = 'Invalid input value'
@@ -42,7 +42,7 @@ class MyTestCase(unittest.TestCase):
             score_input(test_name, test_score)
         self.assertTrue('Invalid test score, try again!' in str(err.exception))
 
-    def test_score_input_invalid_message(self):
+    def test_score_input_invalid_message(self):  # Finally, we include all arguments and validate invalid_message
         with self.assertRaises(ValueError) as err:
             test_name = 'Python Exam 101'
             test_score = -50
