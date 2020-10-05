503173966ac82c3768cae5c7e3184288b14ed854
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-10-05 14:17:09 -0500

    Updated a variable name and added comments.

--- basic_function_assignment/basic_function.py
+++ basic_function_assignment/basic_function.py
@@ -1,38 +1,31 @@
 """
 Program: basic_function.py
 Author: Grayson Hardin
-Last date modified: 9/28/2020
+Last date modified: 10/5/2020
 
 
 The program will take multiple inputs and store it within a list if it is within the parameters (1-100).
 I have ran this program many times and have provided my documentation down below.
 
 """
-"""
-def hourly_employee_input():
-    user_string_input = input("Enter string value: ")
-    user_integer_input = int(input("Enter integer value: "))
-    user_float_input = float(input("Enter float value: "))
-    print(user_string_input, user_integer_input, user_float_input)
-"""
 
 
 def hourly_employee_input():
-    name = input("Enter name: ")
-    age = int(input("Enter age: "))
-    salary = float(input("Enter salary: "))
+    name = input("Enter name: ")  # Gain name input
+    hours_worked = int(input("Enter hours worked: "))  # Gain hours input
+    salary = float(input("Enter salary: "))  # Gain salary input
 
-    if not name.isalpha() or age < 0 or salary < 0:
-        raise ValueError('Value is negative and/or not valid')
+    if not name.isalpha() or hours_worked < 0 or salary < 0:  # This will raise an error if the name does not equal a string, if hours is less than 0 and if salary is less than 0.
+        raise ValueError('Value is negative and/or not valid')  # Programmer can add custom message here.
 
-    print(f'Name is {name}:  age: {age} salary is ${salary} an hour')
+    print(f'Name is {name}:  hours: {hours_worked} salary is ${salary} an hour.')  # formatted string
 
 
 def main():
     try:
         hourly_employee_input()
     except ValueError as err:
-        print('Invalid input: ', err)
+        print('Invalid input: ', err)  # prints error message
 
 
 if __name__ == '__main__':
