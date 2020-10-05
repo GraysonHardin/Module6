069dab166917c89c7dd9323ec55254204dd64204
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-10-04 19:23:57 -0500

    Added documentation and docstring.

--- source_files/inner_functions_assignment.py
+++ source_files/inner_functions_assignment.py
@@ -1,30 +1,31 @@
 """
-Grayson Hardin
 Program: inner_functions_assignment.py
-Date Last Modified: 10/4/2020
+Author: Grayson Hardin
+Last date modified: 10/4/2020
 
+inner_functions_assignment will take the measurements for a rectangle and a square. It will return the perimeter and area of both objects in a formatted string.
 """
 
 
-def measurements(list_of_measurements):
+def measurements(list_of_measurements):  # call parameter
     def area(a_list):
-        is_rectangle = len(a_list) == 2
+        is_rectangle = len(a_list) == 2  # Setting the list default value to 2. While line 12 and 13 could be made into a single, I thought this made the most logical sense
         if is_rectangle:
-            return a_list[0] * a_list[1]
+            return a_list[0] * a_list[1]  # The math then multiplies accordingly
         return a_list[0] * a_list[0]
 
     def perimeter(a_list):
-        is_rectangle = len(a_list) == 2
+        is_rectangle = len(a_list) == 2  # Same concept as previously outlined
         if is_rectangle:
-            return a_list[0] * 2 + a_list[1] * 2
+            return a_list[0] * 2 + a_list[1] * 2  # Unlike line 14, this has to take into account all four sides
         return a_list[0] * 4
 
-    return f'Perimeter = {perimeter(list_of_measurements)} Area = {area(list_of_measurements)}'
+    return f'Perimeter = {perimeter(list_of_measurements)} Area = {area(list_of_measurements)}'  # Formatted string to return appropriate values in custom message
 
 
 def main():
-    rectangle = measurements(list_of_measurements=[3.5, 4])
-    square = measurements(list_of_measurements=[4])
+    rectangle = measurements(list_of_measurements=[3.5, 4])  # Use this to set values for rectangle
+    square = measurements(list_of_measurements=[4])  # Use this to set values for square
     print(rectangle)
     print(square)
 
--- test_functions/test_inner_functions.py
+++ test_functions/test_inner_functions.py
@@ -4,10 +4,10 @@ from source_files.inner_functions_assignment import measurements
 
 class MyTestCase(unittest.TestCase):
     def test_measurements_rectangle(self):
-        self.assertEqual(measurements([2.1, 3.4]), "Perimeter = 11.0 Area = 7.14")
+        self.assertEqual(measurements([2.1, 3.4]), "Perimeter = 11.0 Area = 7.14")  # Test coverage for rectangle
 
     def test_measurements_square(self):
-        self.assertEqual(measurements([3.5]), "Perimeter = 14.0 Area = 12.25")
+        self.assertEqual(measurements([3.5]), "Perimeter = 14.0 Area = 12.25")  # Test coverage for square
 
 
 if __name__ == '__main__':
