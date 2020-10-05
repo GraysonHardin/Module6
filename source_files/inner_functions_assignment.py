57a6a14f35caa4de6f02e2524711207c13f43f93
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-10-04 19:13:11 -0500

    All tests work and code functions as intended.

--- source_files/inner_functions_assignment.py
+++ source_files/inner_functions_assignment.py
@@ -1,7 +1,7 @@
 """
 Grayson Hardin
 Program: inner_functions_assignment.py
-Date Last Modified: 10/2/2020
+Date Last Modified: 10/4/2020
 
 """
 
@@ -11,18 +11,22 @@ def measurements(list_of_measurements):
         is_rectangle = len(a_list) == 2
         if is_rectangle:
             return a_list[0] * a_list[1]
+        return a_list[0] * a_list[0]
 
     def perimeter(a_list):
         is_rectangle = len(a_list) == 2
         if is_rectangle:
             return a_list[0] * 2 + a_list[1] * 2
+        return a_list[0] * 4
 
     return f'Perimeter = {perimeter(list_of_measurements)} Area = {area(list_of_measurements)}'
 
 
 def main():
     rectangle = measurements(list_of_measurements=[3.5, 4])
+    square = measurements(list_of_measurements=[4])
     print(rectangle)
+    print(square)
 
 
 if __name__ == '__main__':
