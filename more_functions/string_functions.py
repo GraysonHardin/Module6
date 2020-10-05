8615f38cdae6e3277d810fb4d82353ad3be8cfde
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-10-05 14:37:00 -0500

    Code functions as intended.

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -1,2 +1,23 @@
-def multiple_string():
-    pass
+"""
+Program: string_functions.py
+Author: Grayson Hardin
+Last date modified: 10/5/2020
+
+This will take a string, a number, and multiple them together. Testing was included as well.
+"""
+
+
+def multiple_string(message, n):
+    calculation = message * n
+    return calculation
+
+
+def main():
+    message = input("Enter a string: ")
+    n = int(input("Enter a number: "))
+    calculation = multiple_string(message, n)
+    print(calculation)
+
+
+if __name__ == '__main__':
+    main()
--- test_functions/test_string_functions.py
+++ test_functions/test_string_functions.py
@@ -4,10 +4,9 @@ from more_functions.string_functions import multiple_string
 
 class MyTestCase(unittest.TestCase):
     def test_multiple_string(self):
-        user_name = 'Grayson'
-        expected = user_name * 3
+        expected = 'BobBobBob'
 
-        actual = multiple_string(user_name * 3)
+        actual = multiple_string(message='Bob', n=3)
         self.assertEqual(expected, actual)
 
 
